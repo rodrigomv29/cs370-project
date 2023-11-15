279242dbe1a83e42809ccee101e6e57c88dfa2da
rodrigomv29 <43789876+rodrigomv29@users.noreply.github.com>
2023-11-15 14:59:11 -0500

    text to speech features added

--- main.py
+++ main.py
@@ -7,12 +7,26 @@ def bracketIsApplause(bracket):
     return bracket == "[Applause]"
 def bracketIsLaughter(bracket):
     return bracket == "[Laughter]"
+def writeToFile(s, name):
+    path ="./transcripts/"
+    filename = name +".txt"
+    path+=filename
+    f1 = open(path, "w")
+    f1.write(s)
+    f1.close()
 id_list=[
 "G3Eup4mfJdA","480OGItLZNo",
 "OA2Tj75T3fI", "qrvK_KuIeJk",
 "oFVuQ0RP_As", "4aPp8KX6EiU",
 "h8PSWeRLGXs", "Z8qC2tVkGeU",
 "Y9nM_9oBj2k", "ervLwxz7xPo"
+]
+title_list =[
+"kamala-harris", "pink",
+"joe-biden", "rich-paul",
+"geoffrey-hinton", "mark-millie",
+"merrick-garland", "deion-sanders",
+"volodymyr zelenskyy", "denzel-washington"
 ]
 
 def getTranscriptStr(video_id):
@@ -34,8 +48,11 @@ def getTranscriptStr(video_id):
                 isInsideBracket = False
                 final_trans_str += " EXPLETIVE "
         else:
-            final_trans_str += i;
+            final_trans_str += i
     return final_trans_str;
-## enter video_id as input
-v_id = input("Enter a video id:")
-print(getTranscriptStr(v_id))
+
+if __name__ == '__main__':
+    for i in range(len(id_list)):
+        v_id = id_list[i]
+        solution = getTranscriptStr(v_id)
+        writeToFile(solution, title_list[i])
