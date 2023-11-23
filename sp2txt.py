d72975e0b8db95d35fa4d308e27e6f5c65ab7870
rodrigomv29 <43789876+rodrigomv29@users.noreply.github.com>
2023-11-23 08:52:40 -0500

    sp2txt features added

--- sp2txt.py
+++ sp2txt.py
@@ -1,2 +1,5 @@
 import whisper
-print("hello world")
+model = whisper.load_model("base")
+result = model.transcribe("C:/Users/rodri/Documents/edu/njit/cs370/final_project/mp3/kamala-harris.mp3")
+print(result["text"])
+
